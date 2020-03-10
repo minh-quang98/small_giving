23d50f539aec9cb38675c371299ed048317fba4e
minhquang <barondark998@gmail.com>
2020-03-10 12:11:50 +0700

    update by quang

--- src/pages/NewsPage.js
+++ src/pages/NewsPage.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap';
 
 import { getColor } from 'utils/colors';
 import { randomNum } from 'utils/demos';
@@ -6,15 +13,91 @@ import { randomNum } from 'utils/demos';
 import { Row, Col, Card, CardHeader, CardBody } from 'reactstrap';
 
 import { Line, Pie, Doughnut, Bar, Radar, Polar } from 'react-chartjs-2';
-
 import Page from 'components/Page';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/js/src/util'
 
 
 class NewsPage extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeIndex: 0,
+      animating: false,
+      fakeData : [
+        {
+          src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa1d%20text%20%7B%20fill%3A%23555%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa1d%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22285.921875%22%20y%3D%22218.3%22%3EFirst%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
+          altText: 'Slide 1',
+          caption: 'Slide 1'
+        },
+        {
+          src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa20%20text%20%7B%20fill%3A%23444%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa20%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23666%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22247.3203125%22%20y%3D%22218.3%22%3ESecond%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
+          altText: 'Slide 2',
+          caption: 'Slide 2'
+        },
+        {
+          src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa21%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa21%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22277%22%20y%3D%22218.3%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
+          altText: 'Slide 3',
+          caption: 'Slide 3'
+        }
+      ]
+    }
+  }
+
+  nextPage () {
+    if (!this.state.animating) {
+      this.setState({
+        activeIndex: this.state.activeIndex === this.state.fakeData.length - 1 ? 0 : this.state.activeIndex + 1
+      })
+    }
+  }
+
+
+  previousPage () {
+    if(!this.state.animating) {
+      this.setState({
+        activeIndex: this.state.activeIndex === 0 ? this.state.fakeData.length - 1 : this.state.activeIndex - 1
+      })
+    }
+  }
+
+  goToIndex = (newIndex) => {
+    if (this.state.animating) return;
+    this.setState({
+      activeIndex: newIndex
+    })
+  }
+
   render () {
+    let {fakeData, activeIndex} = this.state
     return (
       <Page title="Tin tức" >
-
+          <div>
+            <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+              <div className="carousel-inner">
+                <div className="carousel-item active">
+                  <img className="d-block w-100" src="https://innotour.vn/wp-content/uploads/2017/11/phong-canh-dep-2-814x478.jpg"
+                       alt="First slide"/>
+                </div>
+                <div className="carousel-item">
+                  <img className="d-block w-100" src="https://img.thuthuatphanmem.vn/uploads/2018/10/08/hinh-anh-tranh-phong-canh-dep_093819984.jpg"
+                       alt="Second slide"/>
+                </div>
+                <div className="carousel-item">
+                  <img className="d-block w-100" src="https://innotour.vn/wp-content/uploads/2017/11/phong-canh-dep-2-814x478.jpg"
+                       alt="Third slide"/>
+                </div>
+              </div>
+              <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="sr-only">Previous</span>
+              </a>
+              <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="sr-only">Next</span>
+              </a>
+            </div>
+          </div>
       </Page>
     );
   }
