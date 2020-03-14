0d29ab936c1c8d98cd0b166a81e577155a24f01c
minhquang <barondark998@gmail.com>
2020-03-14 17:43:11 +0700

    update guild page

--- src/pages/GuidePage.js
+++ src/pages/GuidePage.js
@@ -1,340 +1,76 @@
 import Page from 'components/Page';
 import React from 'react';
 import { Card, CardBody, CardHeader, Col, Row, Table } from 'reactstrap';
+import Media from 'reactstrap/es/Media';
+import { MdAccountBox } from "react-icons/md";
+import moment from 'moment';
+import NumberFormat from "react-number-format";
 
-const tableTypes = ['', 'bordered', 'striped', 'hover'];
+// const tableTypes = ['', 'bordered', 'striped', 'hover'];
 
 const GuidePage = () => {
   return (
     <Page
       title="Hướng dẫn nạp tiền"
     >
-      {/*{tableTypes.map((tableType, index) => (*/}
-      {/*  <Row key={index}>*/}
-      {/*    <Col>*/}
-      {/*      <Card className="mb-3">*/}
-      {/*        <CardHeader>{tableType || 'default'}</CardHeader>*/}
-      {/*        <CardBody>*/}
-      {/*          <Row>*/}
-      {/*            <Col>*/}
-      {/*              <Card body>*/}
-      {/*                <Table {...{ [tableType || 'default']: true }}>*/}
-      {/*                  <thead>*/}
-      {/*                    <tr>*/}
-      {/*                      <th>#</th>*/}
-      {/*                      <th>First Name</th>*/}
-      {/*                      <th>Last Name</th>*/}
-      {/*                      <th>Username</th>*/}
-      {/*                    </tr>*/}
-      {/*                  </thead>*/}
-      {/*                  <tbody>*/}
-      {/*                    <tr>*/}
-      {/*                      <th scope="row">1</th>*/}
-      {/*                      <td>Mark</td>*/}
-      {/*                      <td>Otto</td>*/}
-      {/*                      <td>@mdo</td>*/}
-      {/*                    </tr>*/}
-      {/*                    <tr>*/}
-      {/*                      <th scope="row">2</th>*/}
-      {/*                      <td>Jacob</td>*/}
-      {/*                      <td>Thornton</td>*/}
-      {/*                      <td>@fat</td>*/}
-      {/*                    </tr>*/}
-      {/*                    <tr>*/}
-      {/*                      <th scope="row">3</th>*/}
-      {/*                      <td>Larry</td>*/}
-      {/*                      <td>the Bird</td>*/}
-      {/*                      <td>@twitter</td>*/}
-      {/*                    </tr>*/}
-      {/*                  </tbody>*/}
-      {/*                </Table>*/}
-      {/*              </Card>*/}
-      {/*            </Col>*/}
-
-      {/*            <Col>*/}
-      {/*              <Card body>*/}
-      {/*                <Table dark>*/}
-      {/*                  <thead>*/}
-      {/*                    <tr>*/}
-      {/*                      <th>#</th>*/}
-      {/*                      <th>First Name</th>*/}
-      {/*                      <th>Last Name</th>*/}
-      {/*                      <th>Username</th>*/}
-      {/*                    </tr>*/}
-      {/*                  </thead>*/}
-      {/*                  <tbody>*/}
-      {/*                    <tr>*/}
-      {/*                      <th scope="row">1</th>*/}
-      {/*                      <td>Mark</td>*/}
-      {/*                      <td>Otto</td>*/}
-      {/*                      <td>@mdo</td>*/}
-      {/*                    </tr>*/}
-      {/*                    <tr>*/}
-      {/*                      <th scope="row">2</th>*/}
-      {/*                      <td>Jacob</td>*/}
-      {/*                      <td>Thornton</td>*/}
-      {/*                      <td>@fat</td>*/}
-      {/*                    </tr>*/}
-      {/*                    <tr>*/}
-      {/*                      <th scope="row">3</th>*/}
-      {/*                      <td>Larry</td>*/}
-      {/*                      <td>the Bird</td>*/}
-      {/*                      <td>@twitter</td>*/}
-      {/*                    </tr>*/}
-      {/*                  </tbody>*/}
-      {/*                </Table>*/}
-      {/*              </Card>*/}
-      {/*            </Col>*/}
-      {/*          </Row>*/}
-      {/*        </CardBody>*/}
-      {/*      </Card>*/}
-      {/*    </Col>*/}
-      {/*  </Row>*/}
-      {/*))}*/}
-
-      {/*<Row>*/}
-      {/*  <Col>*/}
-      {/*    <Card className="mb-3">*/}
-      {/*      <CardHeader>Contextual</CardHeader>*/}
-      {/*      <CardBody>*/}
-      {/*        <Table>*/}
-      {/*          <thead>*/}
-      {/*            <tr>*/}
-      {/*              <th scope="col">Type</th>*/}
-      {/*              <th scope="col">Column heading</th>*/}
-      {/*              <th scope="col">Column heading</th>*/}
-      {/*              <th scope="col">Column heading</th>*/}
-      {/*            </tr>*/}
-      {/*          </thead>*/}
-      {/*          <tbody>*/}
-      {/*            <tr className="table-active">*/}
-      {/*              <th scope="row">Active</th>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*            </tr>*/}
-      {/*            <tr>*/}
-      {/*              <th scope="row">Default</th>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*            </tr>*/}
-
-      {/*            <tr className="table-primary">*/}
-      {/*              <th scope="row">Primary</th>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*            </tr>*/}
-      {/*            <tr className="table-secondary">*/}
-      {/*              <th scope="row">Secondary</th>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*            </tr>*/}
-      {/*            <tr className="table-success">*/}
-      {/*              <th scope="row">Success</th>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*            </tr>*/}
-      {/*            <tr className="table-danger">*/}
-      {/*              <th scope="row">Danger</th>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*            </tr>*/}
-      {/*            <tr className="table-warning">*/}
-      {/*              <th scope="row">Warning</th>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*            </tr>*/}
-      {/*            <tr className="table-info">*/}
-      {/*              <th scope="row">Info</th>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*            </tr>*/}
-      {/*            <tr className="table-light">*/}
-      {/*              <th scope="row">Light</th>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*            </tr>*/}
-      {/*            <tr className="table-dark">*/}
-      {/*              <th scope="row">Dark</th>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*            </tr>*/}
-      {/*          </tbody>*/}
-      {/*        </Table>*/}
-      {/*      </CardBody>*/}
-      {/*    </Card>*/}
-      {/*  </Col>*/}
-
-      {/*  <Col>*/}
-      {/*    <Card className="mb-3">*/}
-      {/*      <CardHeader>Contextual</CardHeader>*/}
-      {/*      <CardBody>*/}
-      {/*        <Table dark>*/}
-      {/*          <thead>*/}
-      {/*            <tr>*/}
-      {/*              <th scope="col">Type</th>*/}
-      {/*              <th scope="col">Column heading</th>*/}
-      {/*              <th scope="col">Column heading</th>*/}
-      {/*              <th scope="col">Column heading</th>*/}
-      {/*            </tr>*/}
-      {/*          </thead>*/}
-      {/*          <tbody>*/}
-      {/*            <tr className="table-active">*/}
-      {/*              <th scope="row">Active</th>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*            </tr>*/}
-      {/*            <tr>*/}
-      {/*              <th scope="row">Default</th>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*            </tr>*/}
-
-      {/*            <tr className="table-primary">*/}
-      {/*              <th scope="row">Primary</th>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*            </tr>*/}
-      {/*            <tr className="table-secondary">*/}
-      {/*              <th scope="row">Secondary</th>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*            </tr>*/}
-      {/*            <tr className="table-success">*/}
-      {/*              <th scope="row">Success</th>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*            </tr>*/}
-      {/*            <tr className="table-danger">*/}
-      {/*              <th scope="row">Danger</th>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*            </tr>*/}
-      {/*            <tr className="table-warning">*/}
-      {/*              <th scope="row">Warning</th>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*            </tr>*/}
-      {/*            <tr className="table-info">*/}
-      {/*              <th scope="row">Info</th>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*            </tr>*/}
-      {/*            <tr className="table-light">*/}
-      {/*              <th scope="row">Light</th>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*            </tr>*/}
-      {/*            <tr className="table-dark">*/}
-      {/*              <th scope="row">Dark</th>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*              <td>Column content</td>*/}
-      {/*            </tr>*/}
-      {/*          </tbody>*/}
-      {/*        </Table>*/}
-      {/*      </CardBody>*/}
-      {/*    </Card>*/}
-      {/*  </Col>*/}
-      {/*</Row>*/}
-
-      {/*<Row>*/}
-      {/*  <Col>*/}
-      {/*    <Card className="mb-3">*/}
-      {/*      <CardHeader>Responsive</CardHeader>*/}
-      {/*      <CardBody>*/}
-      {/*        <Table responsive>*/}
-      {/*          <thead>*/}
-      {/*            <tr>*/}
-      {/*              <th>#</th>*/}
-      {/*              <th>First Name</th>*/}
-      {/*              <th>Last Name</th>*/}
-      {/*              <th>Username</th>*/}
-      {/*            </tr>*/}
-      {/*          </thead>*/}
-      {/*          <tbody>*/}
-      {/*            <tr>*/}
-      {/*              <th scope="row">1</th>*/}
-      {/*              <td>Mark</td>*/}
-      {/*              <td>Otto</td>*/}
-      {/*              <td>@mdo</td>*/}
-      {/*            </tr>*/}
-      {/*            <tr>*/}
-      {/*              <th scope="row">2</th>*/}
-      {/*              <td>Jacob</td>*/}
-      {/*              <td>Thornton</td>*/}
-      {/*              <td>@fat</td>*/}
-      {/*            </tr>*/}
-      {/*            <tr>*/}
-      {/*              <th scope="row">3</th>*/}
-      {/*              <td>Larry</td>*/}
-      {/*              <td>the Bird</td>*/}
-      {/*              <td>@twitter</td>*/}
-      {/*            </tr>*/}
-      {/*          </tbody>*/}
-      {/*        </Table>*/}
-      {/*      </CardBody>*/}
-      {/*    </Card>*/}
-      {/*  </Col>*/}
-      {/*</Row>*/}
-
-      {/*<Row>*/}
-      {/*  <Col>*/}
-      {/*    <Card className="mb-3">*/}
-      {/*      <CardHeader>Size</CardHeader>*/}
-      {/*      <CardBody>*/}
-      {/*        <Table size="sm">*/}
-      {/*          <thead>*/}
-      {/*            <tr>*/}
-      {/*              <th>#</th>*/}
-      {/*              <th>First Name</th>*/}
-      {/*              <th>Last Name</th>*/}
-      {/*              <th>Username</th>*/}
-      {/*            </tr>*/}
-      {/*          </thead>*/}
-      {/*          <tbody>*/}
-      {/*            <tr>*/}
-      {/*              <th scope="row">1</th>*/}
-      {/*              <td>Mark</td>*/}
-      {/*              <td>Otto</td>*/}
-      {/*              <td>@mdo</td>*/}
-      {/*            </tr>*/}
-      {/*            <tr>*/}
-      {/*              <th scope="row">2</th>*/}
-      {/*              <td>Jacob</td>*/}
-      {/*              <td>Thornton</td>*/}
-      {/*              <td>@fat</td>*/}
-      {/*            </tr>*/}
-      {/*            <tr>*/}
-      {/*              <th scope="row">3</th>*/}
-      {/*              <td>Larry</td>*/}
-      {/*              <td>the Bird</td>*/}
-      {/*              <td>@twitter</td>*/}
-      {/*            </tr>*/}
-      {/*          </tbody>*/}
-      {/*        </Table>*/}
-      {/*      </CardBody>*/}
-      {/*    </Card>*/}
-      {/*  </Col>*/}
-      {/*</Row>*/}
+      <Row>
+        <Col xl={12} lg={12} md={12}>
+          <Media
+            object
+            src={""}
+            className="rounded mr-2 mb-2"
+            style={{ width: '100%', height: '100%' }}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col xl={3} lg={3} md={12}>
+          <Card className="d-flex flex-column align-items-center pt-4 pb-4">
+            <div >
+              <MdAccountBox style={{width: 50, height: 50}}/>
+            </div>
+            <div className="text-center">
+              <div style={{fontSize:20}}>Nguyễn Thị A</div>
+              <div>098989898</div>
+              <div>abc@gmail.com</div>
+            </div>
+          </Card>
+        </Col>
+        <Col xl={9} lg={9} md={12}>
+          <Card className="p-2">
+            <Table bordered responsive>
+                <tbody>
+                  <tr>
+                    <th className="text-center">Chủ thẻ</th>
+                    <td className="text-center">Small Giving</td>
+                  </tr>
+                  <tr>
+                    <th className="text-center">Số tài khoản</th>
+                    <td className="text-center">1818173682</td>
+                  </tr>
+                  <tr>
+                    <th className="text-center">Tên ngân hàng</th>
+                    <td className="text-center">BIDV</td>
+                  </tr>
+                </tbody>
+            </Table>
+          </Card>
+        </Col>
+        <Col xl={12} lg={12} md={12}>
+          <Card>
+            <CardHeader className="text-center" style={{fontSize: 22}}>
+              Cách thức nạp tiền
+            </CardHeader>
+            <CardBody>
+              <div style={{fontSize: 30 }}>
+                <div>Bước 1: Chuyển tiền vào số tải khoản trên</div>
+                <div>Bước 2: Nội dung chuyển tiền (Số điện thoại bạn đăng ký)</div>
+                <div className="text-center">098989898</div>
+              </div>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
     </Page>
   );
 };
