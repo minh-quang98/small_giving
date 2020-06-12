bd415ceda1472ac379154d393fe53f138a921f02
hongphan911 <hong.phan9119@gmail.com>
2020-06-12 16:15:56 +0700

    update

--- src/pages/admin/tintucsua.js
+++ src/pages/admin/tintucsua.js
@@ -15,7 +15,8 @@ import {
   Container,
 } from 'reactstrap';
 import { withSnackbar } from 'notistack';
-
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import CKEditor from '@ckeditor/ckeditor5-react';
 
 const initialState = {
   id: '',
